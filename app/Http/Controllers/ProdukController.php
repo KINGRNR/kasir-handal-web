196b7794b29ea78d991f0363bfbd3f5c68138e50
Rafif <rafifnr06@gmail.com>
2024-02-05 19:21:46 +0700

    update api save(create update produk)

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Yajra\DataTables\Facades\DataTables;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Validator;
 
 class ProdukController extends Controller
 {
@@ -19,7 +21,7 @@ class ProdukController extends Controller
     // $id_toko = DB::table('toko')->where('toko_user_id', $id)->select('toko_id')->first();
 
     // $operation = DB::table('users')->where('users_role_id','TKQR2DSJlQ5b31V2')->get();
-    $operation = DB::table('v_produk')->where('id_kategori_toko', $id)->get();
+    $operation = DB::table('v_produk')->where('id_kategori_toko', $id)->where('produk_deleted_at', null)->get();
 
     return DataTables::of($operation)
       ->toJson();
@@ -32,10 +34,9 @@ class ProdukController extends Controller
     // $id_toko = DB::table('toko')->where('toko_user_id', $id)->select('toko_id')->first();
 
     // $operation = DB::table('users')->where('users_role_id','TKQR2DSJlQ5b31V2')->get();
-    $operation = DB::table('v_produk')->where('id_kategori_toko', $id)->get();
+    $operation = DB::table('v_produk')->where('id_kategori_toko', $id)->where('produk_deleted_at', null)->get();
 
     return response()->json($operation);
-
   }
 
   public function getKategori()
@@ -45,57 +46,133 @@ class ProdukController extends Controller
     $data = DB::table('kategori')->where('id_kategori_toko', $id)->where('kategori_deleted_at', null)->get();
     return response()->json($data);
   }
+
+  public function saveMob(Request $request)
+  {
+    $validator = Validator::make($request->all(), [
+      'foto_produk' => 'image|mimes:jpeg,png,jpg,gif|max:2048', // Max size 2MB
+    ]);
+
+    if ($validator->fails()) {
+      return response()->json([
+        'success' => false,
+        'status' => 'Validation Error',
+        'title' => 'Gagal!',
+        'message' => 'Validasi tidak berhasil. Pastikan gambar berformat JPEG, PNG, atau GIF dan tidak lebih dari 2MB.',
+        'code' => 422,
+        'errors' => $validator->errors(),
+      ]);
+    }
+
+    $formData = $request->except('foto_produk');
+
+    if ($request->hasFile('foto_produk')) {
+      $uploadedFile = $request->file('foto_produk');
+
+      $filename = 'produk_' . time() . '.' . $uploadedFile->getClientOriginalExtension();
+
+      $uploadedFile->move(public_path('file/produk_foto/'), $filename);
+      $formData['foto_produk'] = $filename;
+    }
+
+    $formData['harga_produk'] = str_replace(',', '', str_replace('.', '', $formData['harga_produk']));
+
+    if ($formData['id_produk']) {
+      $existingProduk = Produk::find($formData['id_produk']);
+
+      if ($request->hasFile('foto_produk')) {
+        // Remove the old image
+        if (file_exists(public_path('file/produk_foto/') . $existingProduk->foto_produk)) {
+          unlink(public_path('file/produk_foto/') . $existingProduk->foto_produk);
+        }
+
+        // Update with the new image
+        $existingProduk->update($formData);
+      } else {
+        // If no new image, update without changing the existing image
+        $existingProduk->update($request->except('foto_produk'));
+      }
+    } else {
+      // Create a new record
+      Produk::create($formData);
+    }
+
+    return response()->json([
+      'success' => true,
+      'status' => 'Success',
+      'title' => 'Sukses!',
+      'message' => 'Data Berhasil Tersimpan!',
+      'code' => 201
+    ]);
+  }
+
+
+
   public function save(Request $request)
   {
     // try {
-      $formData = $request->except('croppedPhoto');
-      $base64Image = $request->input('croppedPhoto');
-      $imageData = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $base64Image));
-      $filename = 'produk_' . time() . '.png';
-      $path = public_path('file/produk_foto/' . $filename);
-      $formData['harga_produk'] = str_replace(',', '', str_replace('.', '', $formData['harga_produk']));
+    $formData = $request->except('croppedPhoto');
+    $base64Image = $request->input('croppedPhoto');
+    $imageData = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $base64Image));
+    $filename = 'produk_' . time() . '.png';
+    $path = public_path('file/produk_foto/' . $filename);
+    $formData['harga_produk'] = str_replace(',', '', str_replace('.', '', $formData['harga_produk']));
 
-      file_put_contents($path, $imageData);
-  
-      if ($formData['id_produk']) {
-          // $kategori = Kategori::findOrFail($data['id_kategori']);
-          // $kategori->update($data);
-      } else {
-          $formData['id_produk_kategori'] = $formData['kategori_produk'];
-          $formData['foto_produk'] = $filename;
+    file_put_contents($path, $imageData);
 
-          Produk::create($formData);
-      }
+    if ($formData['id_produk']) {
+      // $kategori = Kategori::findOrFail($data['id_kategori']);
+      // $kategori->update($data);
+    } else {
+      $formData['id_produk_kategori'] = $formData['kategori_produk'];
+      $formData['foto_produk'] = $filename;
 
-      return response()->json([
-          'success' =>  true,
-          'status' =>  'Success',
-          'title' => 'Sukses!',
-          'message' => 'Data Berhasil Tersimpan!',
-          'code' => 201
-      ]);
-  // } catch (\Throwable $th) {
-  //     return response()->json([
-  //         'success' =>  false,
-  //         'status' =>  'error',
-  //         'title' => 'Gagal!',
-  //         'message' => 'Terjadi Kesalahan di Sistem!',
-  //     ]);
-  // }
+      Produk::create($formData);
+    }
+
+    return response()->json([
+      'success' =>  true,
+      'status' =>  'Success',
+      'title' => 'Sukses!',
+      'message' => 'Data Berhasil Tersimpan!',
+      'code' => 201
+    ]);
+    // } catch (\Throwable $th) {
+    //     return response()->json([
+    //         'success' =>  false,
+    //         'status' =>  'error',
+    //         'title' => 'Gagal!',
+    //         'message' => 'Terjadi Kesalahan di Sistem!',
+    //     ]);
+    // }
   }
-  public function showProdukCart(Request $request) 
+  public function showProdukCart(Request $request)
   {
     $id = session()->get('toko_id');
-    $operation = DB::table('v_produk')->where('id_kategori_toko', $id)->get();
+    $operation = DB::table('v_produk')->where('id_kategori_toko', $id)->where('produk_deleted_at', null)->get();
 
     return response()->json($operation);
   }
-  public function addCart(Request $request) 
+  public function addCart(Request $request)
   {
     $id = $request->post();
     $operation = DB::table('v_produk')->where('id_produk', $id)->first();
 
     return response()->json($operation);
+  }
+  public function delete(Request $request)
+  {
+      $data = $request->post();
+      $id = $data['id'];
+
+      $produk = Produk::find($id);
+
+      if (!$produk) {
+          return response()->json(['error' => 'Produk not found.'], 404);
+      }
+      $produk->delete();
+
+      return response()->json(['success' => 'Produk deleted successfully.']);
   }
 }
   
--- app/Models/Produk.php
+++ app/Models/Produk.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Produk extends Model
 {
+    use SoftDeletes;
+
     protected $table         = 'produk';
     protected $primaryKey    = 'id_produk';
     protected $useAutoIncrement = false;
@@ -20,6 +23,8 @@ class Produk extends Model
     ];
     const CREATED_AT = 'produk_created_at';
     const UPDATED_AT = 'produk_updated_at';
+    const DELETED_AT = 'produk_deleted_at';
+
     public function kategori()
     {
         return $this->belongsTo(Kategori::class, 'id_produk_kategori');
